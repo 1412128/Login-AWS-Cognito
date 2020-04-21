20cc4bfed4a4343d344ea0d0f829692dc039d14a
dang.doan <1412128@student.hcmus.edu.vn>
2020-04-21 09:53:24 +0700

    Updates

---
+++ precache-manifest.2a4e9c60384a9c223ad07dc261232ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3102f871a3eed5e0850b10af39c09aa6",
+    "url": "/Login-AWS-Cognito/index.html"
+  },
+  {
+    "revision": "7c3e619cd0cafa6cc4c1",
+    "url": "/Login-AWS-Cognito/static/css/main.302ff33d.chunk.css"
+  },
+  {
+    "revision": "27a56395df130a18d7b1",
+    "url": "/Login-AWS-Cognito/static/js/2.b2fbea83.chunk.js"
+  },
+  {
+    "revision": "86e07df7117e077ea4c66f71e3a52b84",
+    "url": "/Login-AWS-Cognito/static/js/2.b2fbea83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3e619cd0cafa6cc4c1",
+    "url": "/Login-AWS-Cognito/static/js/main.6b56a163.chunk.js"
+  },
+  {
+    "revision": "6a8c46c72967662dbfcb",
+    "url": "/Login-AWS-Cognito/static/js/runtime-main.e19393de.js"
+  }
+]);
